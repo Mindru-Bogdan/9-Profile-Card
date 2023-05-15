2582034906b0012f90b7b38ef9cee3a2ce33fbc8
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-05-15 18:51:21 +0300

    Device Friendly Solution

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,26 +1,37 @@
+import BackgroundTop from '../../src/bg-pattern-top.svg';
+import BackgroundBottom from '../../src/bg-pattern-bottom.svg';
+import CardTopImage from '../../src/bg-pattern-card.svg';
+import ProfilePicture from '../../src/image-victor.jpg';
+
 import classes from './Card.module.css';
 
 const Card = () => {
   return (
-    <div>
-      <div>
-        <h1>Victor Crest</h1>
-        <h2>26</h2>
-        <h3>London</h3>
-      </div>
-      <div></div>
-      <div>
-        <div className={classes.stats}>
-          <h1>80K</h1>
-          <p>Followers</p>
+    <div className={classes.background}>
+      <img src={BackgroundTop} className={classes.backgroundTop} />
+      <img src={BackgroundBottom} className={classes.backgroundBottom} />
+      <div className={classes.card}>
+        <img src={CardTopImage} className={classes.cardTopImage} />
+        <img src={ProfilePicture} className={classes.profilePicture} />
+        <div>
+          <h1>Victor Crest</h1>
+          <h2>26</h2>
+          <h3>London</h3>
         </div>
-        <div className={classes.stats}>
-          <h1>803K</h1>
-          <p>Likes</p>
-        </div>
-        <div className={classes.stats}>
-          <h1>1.4K</h1>
-          <p>Photos</p>
+        <div className={classes.line}></div>
+        <div className={classes.allStats}>
+          <div className={classes.stats}>
+            <h1>80K</h1>
+            <p>Followers</p>
+          </div>
+          <div className={classes.stats}>
+            <h1>803K</h1>
+            <p>Likes</p>
+          </div>
+          <div className={classes.stats}>
+            <h1>1.4K</h1>
+            <p>Photos</p>
+          </div>
         </div>
       </div>
     </div>
